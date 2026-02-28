235527bc4696d7a252f1c706edce8e6957a6e573
Avi Kainth <askainth2012@gmail.com>
2026-02-28 23:50:05 +0000

    Push V3 Final Fixes

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
-
-$host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+if(function_exists('apache_getenv')){
+    putenv("DB_HOST=".apache_getenv('DB_HOST'));
+    putenv("DB_NAME=".apache_getenv('DB_NAME'));
+    putenv("DB_USER=".apache_getenv('DB_USER'));
+    putenv("DB_PASS=".apache_getenv('DB_PASS'));
+}
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) die("DB connection failed: " . $conn->connect_error);
